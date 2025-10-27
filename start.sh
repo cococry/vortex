eabf7108014f51b44ef39626c29da1c6ced423f2
cococry <cococry.btw@gmail.com>
2025-10-27 17:00:24 +0100

    removed start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-./build/vortex --verbose -lf &
-sleep 2s 
-WAYLAND_DISPLAY=wayland-2 weston-simple-egl &
-sleep 2s 
-WAYLAND_DISPLAY=wayland-2 weston-simple-egl &
-sleep 2s 
-WAYLAND_DISPLAY=wayland-2 weston-simple-egl &
